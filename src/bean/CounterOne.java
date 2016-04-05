bfc8787855e3780bdb97872ba8633f22683173f0
Temo <temo.kiknavelidze@gmail.com>
2016-04-05 07:28:15 +0400

    ...bean scopes(current SessionScoped - means bean data is not shared during whole application as ApplicationScoped, instead each user has his/her unique session data(value))...

---
+++ src/bean/CounterOne.java
@@ -0,0 +1,28 @@
+package bean;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@SessionScoped
+@ManagedBean
+public class CounterOne implements Serializable{
+    private int value = 0;
+
+    public CounterOne() {
+    }
+
+    public String increment(){
+        value++;
+        return "student_one";
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
